1569f2a3fb65b754b7fe7e199f1c59e14e16a0d3
Maciej <kobraks2@gmail.com>
2017-10-07 17:58:01 +0200

    Fixed some errors added new gui type

--- Jump/GuiInputBox.cpp
+++ Jump/GuiInputBox.cpp
@@ -6,11 +6,9 @@ jump::system::gui::GuiInputBox::GuiInputBox()
 {
 	try
 	{
-		size_ = 1;
-		buffer_ = new char [size_];
-		buffer_[0] = '\0';
+		buffer_ = new Buffer(1);
 	}
-	catch(std::bad_alloc)
+	catch (std::bad_alloc)
 	{
 		throw exception::BadAllocException();
 	}
@@ -24,34 +22,36 @@ jump::system::gui::GuiInputBox::GuiInputBox(GuiItem* parent) : GuiInputBox()
 jump::system::gui::GuiInputBox::GuiInputBox(GuiItem* parent, const std::string& text) : GuiInputBox(parent)
 {
 	set_name(text);
-	size_ = text.size();
-	delete[] buffer_;
+}
+
+jump::system::gui::GuiInputBox::GuiInputBox(GuiItem* parent, const std::string& name, const std::string& text) : GuiInputBox(parent, name)
+{
+	delete buffer_;
 
 	try
 	{
-		buffer_ = new char[size_+1];
-		std::copy(text.begin(), text.end(), buffer_);
-		buffer_[size_ - 1] = '\0';
+		buffer_ = new Buffer(text);
 	}
-	catch(std::bad_alloc)
+	catch (std::bad_alloc)
 	{
 		throw exception::BadAllocException();
 	}
 }
 
-jump::system::gui::GuiInputBox::GuiInputBox(GuiItem* parent, const std::string& text, const sf::Vector2f& position) : GuiInputBox(parent, text)
+jump::system::gui::GuiInputBox::GuiInputBox(GuiItem* parent, const std::string& name, const std::string& text,
+	const sf::Vector2f& position) : GuiInputBox(parent, name, text)
 {
 	set_position(position);
 }
 
-jump::system::gui::GuiInputBox::GuiInputBox(GuiItem* parent, const std::string& text, const sf::Vector2f& position,
-	const sf::Vector2f& size) : GuiInputBox(parent, text, position)
+jump::system::gui::GuiInputBox::GuiInputBox(GuiItem* parent, const std::string& name, const std::string& text,
+	const sf::Vector2f& position, const sf::Vector2f& size) : GuiInputBox(parent, name, text, position)
 {
 	set_size(size);
 }
 
-jump::system::gui::GuiInputBox::GuiInputBox(GuiItem* parent, const std::string& text, const sf::Vector2f& position,
-	const sf::Vector2f& size, const flag_t& flags) : GuiInputBox(parent, text, position, size)
+jump::system::gui::GuiInputBox::GuiInputBox(GuiItem* parent, const std::string& name, const std::string& text,
+	const sf::Vector2f& position, const sf::Vector2f& size, const flag_t& flags) : GuiInputBox(parent, name, text, position, size)
 {
 	set_flags(flags);
 }
@@ -69,7 +69,17 @@ jump::system::gui::GuiInputBox::GuiInputBox(GuiInputBox&& input) noexcept
 
 jump::system::gui::GuiInputBox::~GuiInputBox()
 {
-	delete[] buffer_;
+	delete buffer_;
+}
+
+jump::system::gui::GuiItem* jump::system::gui::GuiInputBox::clone() const
+{
+	return new GuiInputBox(parent(), get_name(), get_text(), get_position(), get_size(), get_flags());
+}
+
+std::string jump::system::gui::GuiInputBox::get_text() const
+{
+	return buffer_->get_text();
 }
 
 jump::system::gui::GuiInputBox& jump::system::gui::GuiInputBox::operator=(const GuiInputBox& input)
@@ -90,14 +100,85 @@ jump::system::gui::GuiInputBox& jump::system::gui::GuiInputBox::operator=(GuiInp
 
 	buffer_ = input.buffer_;
 	input.buffer_ = nullptr;
-	size_ = input.size_;
 
 	return*this;
 }
 
 void jump::system::gui::GuiInputBox::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
-	ImGui::InputText(get_name().c_str(), buffer_, size_);
-	text_ = std::string(buffer_);
-	extend_buffer();
+	ImGui::InputText(get_name().c_str(), buffer_->get_buffer(), buffer_->size());
+	buffer_->set_text(buffer_->get_text());
+}
+
+jump::system::gui::GuiInputBox::Buffer::Buffer(const std::string& text) : Buffer(text.size() + 1)
+{
+	std::copy(text.begin(), text.end(), buffer_);
+	buffer_[size_ - 1] = '\0';
+}
+
+jump::system::gui::GuiInputBox::Buffer::Buffer(const size_t& size) : buffer_(new char[size]), size_(size)
+{
+	buffer_[0] = '\0';
+}
+
+jump::system::gui::GuiInputBox::Buffer::Buffer(const Buffer& buffer)
+{
+	*this = buffer;
+}
+
+jump::system::gui::GuiInputBox::Buffer::Buffer(Buffer&& buffer) noexcept
+{
+	*this = std::move(buffer);
+}
+
+jump::system::gui::GuiInputBox::Buffer& jump::system::gui::GuiInputBox::Buffer::operator=(const Buffer& buffer)
+{
+	resize(buffer.size());
+
+	std::copy(buffer.buffer_, buffer.buffer_ + buffer.size_, buffer_);
+
+	return *this;
+}
+
+jump::system::gui::GuiInputBox::Buffer& jump::system::gui::GuiInputBox::Buffer::operator=(Buffer&& buffer) noexcept
+{
+	size_ = buffer.size_;
+	buffer_ = buffer.buffer_;
+	buffer.buffer_ = nullptr;
+
+	return *this;
+}
+
+jump::system::gui::GuiInputBox::Buffer::~Buffer()
+{
+	delete[] buffer_;
+}
+
+char* jump::system::gui::GuiInputBox::Buffer::get_buffer() const
+{
+	return buffer_;
+}
+
+size_t jump::system::gui::GuiInputBox::Buffer::size() const
+{
+	return size_;
+}
+
+void jump::system::gui::GuiInputBox::Buffer::set_text(const std::string& text)
+{
+	resize(text.size() + 1);
+	std::copy(text.begin(), text.end(), buffer_);
+	buffer_[size_ - 1] = '\0';
+}
+
+std::string jump::system::gui::GuiInputBox::Buffer::get_text() const
+{
+	return std::string(buffer_);
+}
+
+void jump::system::gui::GuiInputBox::Buffer::resize(const size_t& size)
+{
+	delete[] buffer_;
+	buffer_ = new char[size];
+	size_ = size;
 }
--- Jump/GuiInputBox.h
+++ Jump/GuiInputBox.h
@@ -14,10 +14,11 @@ namespace jump
 			public:
 				GuiInputBox();
 				explicit GuiInputBox(GuiItem* parent);
-				GuiInputBox(GuiItem* parent, const std::string& text);
-				GuiInputBox(GuiItem* parent, const std::string& text, const sf::Vector2f& position);
-				GuiInputBox(GuiItem* parent, const std::string& text, const sf::Vector2f& position, const sf::Vector2f& size);
-				GuiInputBox(GuiItem* parent, const std::string& text, const sf::Vector2f& position, const sf::Vector2f& size, const flag_t& flags);
+				GuiInputBox(GuiItem* parent, const std::string& name);
+				GuiInputBox(GuiItem* parent, const std::string& name, const std::string& text);
+				GuiInputBox(GuiItem* parent, const std::string& name, const std::string& text, const sf::Vector2f& position);
+				GuiInputBox(GuiItem* parent, const std::string& name, const std::string& text, const sf::Vector2f& position, const sf::Vector2f& size);
+				GuiInputBox(GuiItem* parent, const std::string& name, const std::string& text, const sf::Vector2f& position, const sf::Vector2f& size, const flag_t& flags);
 				GuiInputBox(const GuiInputBox& input);
 				GuiInputBox(GuiInputBox&& input) noexcept;
 
@@ -25,15 +26,39 @@ namespace jump
 
 				GuiItem* clone() const override;
 
+				std::string get_text() const;
+
 				GuiInputBox& operator= (const GuiInputBox& input);
 				GuiInputBox& operator=(GuiInputBox&& input) noexcept;
 			protected:
 				void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
-				std::string text_;
-				char* buffer_;
-				size_t size_;
 
-				void extend_buffer(const size_t& size);
+			private:
+				class Buffer
+				{
+				public:
+					explicit Buffer(const std::string& text);
+					explicit Buffer(const size_t& size);
+					Buffer(const Buffer& buffer);
+					Buffer(Buffer&& buffer) noexcept;
+
+					Buffer& operator=(const Buffer& buffer);
+					Buffer& operator=(Buffer&& buffer) noexcept;
+
+					~Buffer();
+
+					char* get_buffer() const;
+					size_t size() const;
+
+					void set_text(const std::string& text);
+					std::string get_text() const;
+
+					void resize(const size_t& size);
+
+				private:
+					char* buffer_;
+					size_t size_;
+				} *buffer_;
 			};
 		}
 	}
